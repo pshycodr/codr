f835c3f7b65497b05985bec7ef8b1b27c5b6cb4f
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-06-21 20:49:24 +0530

    fix: dotenv Loading

--- apps/cli-ts/src/llm/gemini.ts
+++ apps/cli-ts/src/llm/gemini.ts
@@ -1,13 +1,12 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-// import { ChatOpenAI } from "@langchain/openai";
 import { config } from "dotenv";
+import path from "path";
 
 
-config();
+const envPath = path.resolve(__dirname, "../../.env");
+config({ path: envPath });
 
 export const llm = new ChatGoogleGenerativeAI ({
   model: "gemini-2.0-flash",
   apiKey: process.env.GEMINI_API_KEY
 });
-
-
